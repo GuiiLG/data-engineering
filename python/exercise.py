fc67ed21a08aff37877060c2802b3a1f5ca9265e
GuiiLG <guiluigiribeiro@gmail.com>
2025-04-24 20:21:10 -0300

    Second_commit

---
+++ python/exercise.py
@@ -0,0 +1,7 @@
+def calcular_potencia(base, expoente):
+    return base**expoente
+b = int(input(f'Digite a base :'))
+e = int(input(f'Digite a base :'))
+
+
+print(f"A potencia de {b} elevado a {e} = {calcular_potencia(b,e)}")
